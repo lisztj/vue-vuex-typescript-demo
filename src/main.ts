eadf23d7ef1fa2e5f83fdab23608b18257837d0a
赵健 <mr.91@foxmail.com>
2017-12-20 17:41:38 +0800

    1

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+// import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 // import MintUI from 'mint-ui'
 import 'mint-ui/lib/style.css'
 import './assets/style.css'
@@ -10,20 +12,24 @@ import store from './store'
 
 import { currency } from './currency'
 
-import { Header, Cell, Checklist, PaletteButton, Button, Swipe, SwipeItem,Field,Actionsheet,Tabbar, TabItem,DatetimePicker,Picker } from 'mint-ui'
-Vue.component(Header.name, Header)
-Vue.component(Cell.name, Cell)
-Vue.component(Checklist.name, Checklist)
-Vue.component(PaletteButton.name, PaletteButton)
+import { Button, Select } from 'element-ui'
+
+
+// import { Header, Cell, Checklist, PaletteButton, Swipe, SwipeItem, Field, Actionsheet, Tabbar, TabItem, DatetimePicker, Picker } from 'mint-ui'
+// Vue.component(Header.name, Header)
+// Vue.component(Cell.name, Cell)
+// Vue.component(Checklist.name, Checklist)
+// Vue.component(PaletteButton.name, PaletteButton)
 Vue.component(Button.name, Button)
-Vue.component(Swipe.name, Swipe)
-Vue.component(SwipeItem.name, SwipeItem)
-Vue.component(Field.name, Field)
-Vue.component(Actionsheet.name, Actionsheet)
-Vue.component(Tabbar.name, Tabbar)
-Vue.component(TabItem.name, TabItem)
-Vue.component(DatetimePicker.name, DatetimePicker)
-Vue.component(Picker.name, Picker)
+Vue.component(Select.name, Select)
+// Vue.component(Swipe.name, Swipe)
+// Vue.component(SwipeItem.name, SwipeItem)
+// Vue.component(Field.name, Field)
+// Vue.component(Actionsheet.name, Actionsheet)
+// Vue.component(Tabbar.name, Tabbar)
+// Vue.component(TabItem.name, TabItem)
+// Vue.component(DatetimePicker.name, DatetimePicker)
+// Vue.component(Picker.name, Picker)
 // Vue.use(MintUI)
 
 Vue.filter('currency', currency)
